2ac76f41c5b4a07e2a7d80188165d5c8c1197f6e
Eric Liang <etlgfx@gmail.com>
2013-03-06 23:37:21 -0500

    quad tree implementation getting closer
    but having some endless loop issues

--- js/quadtree.js
+++ js/quadtree.js
@@ -3,24 +3,97 @@ var qt = (function () {
 	var ts = null;
 	var start_ts = null;
 
-	function QuadTree() {
+	var qt = null;
+
+	function Obj(coords) {
+		this.coords = coords;
 	}
 
+	function QuadTree(bounds) {
+		this.bounds = bounds;
+
+		this.objects = [];
+		this.children = null;
+	}
+
+	QuadTree.maxFill = 2;
+
+	//return all points inside the same box as point
 	QuadTree.prototype.query = function(point) {
+		if (!this.boundsCheck(point)) {
+			return [];
+		}
+
+		if (this.children === null) {
+			return this.objects;
+		}
+
+		var result = [];
+
+		this.children.forEach(function (c) {
+			result.concat(c.query(point));
+		});
+
+		return result;
 	};
 
-	QuadTree.prototype.queryBB = function(point1, point2) {
+	/*
+	QuadTree.prototype.queryBounds = function(bounds) {
 	};
+	*/
 
 	QuadTree.prototype.insert = function(obj) {
+		if (!this.boundsCheck(obj.coords)) {
+			return;
+		}
+
+		if (this.objects.length < QuadTree.maxFill) {
+			this.objects.push(obj);
+			return;
+		}
+
+		if (this.children === null) {
+			this.subdivide();
+		}
+
+		this.children.forEach(function (c) {
+			c.insert(obj);
+		});
+	};
+
+	QuadTree.prototype.boundsCheck = function(point) {
+		if (point[0] < this.bounds[0]) return false;
+		if (point[0] >= this.bounds[2]) return false;
+		if (point[1] < this.bounds[1]) return false;
+		if (point[1] >= this.bounds[3]) return false;
+
+		return true;
 	};
 
 	QuadTree.prototype.subdivide = function() {
+		var size = (this.bounds[2] - this.bounds[0]) / 2;
+
+		this.children = [
+			new QuadTree([this.bounds[0], this.bounds[1], this.bounds[0] + size, this.bounds[1] + size]),
+			new QuadTree([this.bounds[0] + size, this.bounds[1], this.bounds[2], this.bounds[1] + size]),
+			new QuadTree([this.bounds[0], this.bounds[1] + size, this.bounds[0] + size, this.bounds[3]]),
+			new QuadTree([this.bounds[0] + size, this.bounds[1] + size, this.bounds[2], this.bounds[3]])
+		];
+
+		var objs = this.objects.splice(0, QuadTree.maxFill);
+
+		objs.forEach(function (o) {
+			this.children.forEach(function (c) {
+				c.insert(o);
+			});
+		}, this);
 	};
 
 
 	function init(id) {
 		canvas = document.getElementById(id);
+		canvas.width = 512;
+		canvas.height = 512;
         context = canvas.getContext('2d');
 
 		(function() {
@@ -29,21 +102,49 @@ var qt = (function () {
 			window.requestAnimationFrame = requestAnimationFrame;
 		})();
 
+		qt = new QuadTree([0, 0, 512, 512]);
+
 		window.addEventListener('click', function (evt) {
-			mouse[0] = evt.clientX - canvas.offsetLeft;
-			mouse[1] = evt.clientY - canvas.offsetTop;
+			qt.insert(new Obj([evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop]));
+
+			drawQuadTree(qt);
 		}, true);
 
-		requestAnimationFrame(setupTimer);
+		//requestAnimationFrame(setupTimer);
 	}
 
+	/*
 	function setupTimer(newts) {
 		ts = newts;
 		start_ts = newts;
 
 		requestAnimationFrame(draw);
 	}
+	*/
 
+	function drawQuadTree(qt) {
+        context.clearRect(
+			0, 0,
+			canvas.width, canvas.height);
+
+		context.strokeRect(qt.bounds[0], qt.bounds[1], qt.bounds[2], qt.bounds[3]);
+
+		/*
+		qt.objects.forEach(function (o) {
+			context.beginPath();
+			context.arc(o.coords[0], o.coords[1], 3, 0, Math.PI * 2, true);
+			context.closePath();
+
+			context.fill();
+		});
+		*/
+
+		if (qt.children) {
+			qt.children.forEach(drawQuadTree);
+		}
+	}
+
+	/*
 	function draw(newts) {
 		var frame_ts = (newts - ts) / 1000;
 		var elapsed_ts = (newts - start_ts) / 1000;
@@ -53,13 +154,11 @@ var qt = (function () {
 			0, 0,
 			canvas.width, canvas.height);
 
-		//dangles[0].draw(context, frame_ts, elapsed_ts);
-		dangles.forEach(function (d) {
-			d.draw(context, frame_ts, elapsed_ts);
-		});
+		drawQuadTree(qt);
 
 		requestAnimationFrame(draw);
 	}
+	*/
 
 	return {
 		'init': init,

3729e47245555cb7bba1fc2d376a340a31944f86
Eric Liang <etlgfx@gmail.com>
2013-03-09 21:10:03 -0500

    adding cheesy css/js mouse selection for testing queryBB

--- js/main.js
+++ js/main.js
@@ -28,13 +28,44 @@ require(['quadtree'], function (QuadTree) {
 			evt.preventDefault();
 		}, true);
 
+		var mouseStart = null;
+		var mouseDown = false;
+		var selectionDiv = document.getElementById('selection');
+
+		window.addEventListener('mousedown', function (evt) {
+			evt.preventDefault();
+
+			if (evt.which == 1) {
+				mouseStart = [evt.clientX, evt.clientY];
+				mouseDown = true;
+			}
+		}, true);
+
+		window.addEventListener('mousemove', function (evt) {
+			evt.preventDefault();
+			if (mouseDown) {
+				selectionDiv.style.display = 'block';
+				selectionDiv.style.top = mouseStart[1] +'px';
+				selectionDiv.style.left = mouseStart[0] +'px';
+				selectionDiv.style.width = (evt.clientX - mouseStart[0]) +'px';
+				selectionDiv.style.height = (evt.clientY - mouseStart[1]) +'px';
+			}
+		}, true);
+
 		window.addEventListener('mouseup', function (evt) {
 			var coords = [evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop];
 			evt.preventDefault();
 
 			switch (evt.which) {
 				case 1: //left click
-					qt.insert(coords, {});
+					if (mouseDown) {
+						console.log(
+							[mouseStart[0] - canvas.offsetLeft, mouseStart[1] - canvas.offsetTop], 
+							coords);
+					}
+					else {
+						qt.insert(coords, {});
+					}
 					break;
 				case 3: //right click
 					selection = qt.query(coords);
@@ -46,6 +77,9 @@ require(['quadtree'], function (QuadTree) {
 				canvas.width, canvas.height);
 
 			drawQuadTree(qt);
+			mouseStart = null;
+			mouseDown = false;
+			selectionDiv.style.display = 'none';
 		}, true);
 
 		//requestAnimationFrame(setupTimer);

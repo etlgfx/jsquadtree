8a1cbc660ca576f6b79939ae7509a8e29329aae4
Eric Liang <etlgfx@gmail.com>
2013-03-17 13:57:50 -0400

    making some more realistc tests

--- test/node.js
+++ test/node.js
@@ -4,7 +4,8 @@ var QuadTree = require('../js/quadtree');
 
 var x = 0, y = 0, size = 512;
 
-assert.throws(new QuadTree(), 'x,y,size are required');
+assert.throws(function () { new QuadTree() }, 'x,y,size are required');
+assert.throws(function () { new QuadTree([0, 0]) }, 'x,y,size are required');
 
 var qt = new QuadTree({x: x, y: y, size: size});
 
@@ -12,5 +13,11 @@ assert.equal(qt.bounds[0], x);
 assert.equal(qt.bounds[1], y);
 assert.equal(qt.bounds[2], x + size);
 assert.equal(qt.bounds[3], y + size);
+assert.equal(qt.config.maxDepth, QuadTree.maxDepth);
+assert.equal(qt.config.maxFill, QuadTree.maxFill);
+
+var qt = new QuadTree({x: x, y: y, size: size, maxDepth: 1, maxFill: 1});
+assert.equal(qt.config.maxDepth, 1);
+assert.equal(qt.config.maxFill, 1);
 
 console.log(qt);

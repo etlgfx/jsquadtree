d289cd94ecd734e1fa730c3192a1c0a7d5e012a1
Eric Liang <etlgfx@gmail.com>
2013-03-17 14:17:54 -0400

    making tests more useful

--- test/node.js
+++ test/node.js
@@ -9,15 +9,41 @@ assert.throws(function () { new QuadTree([0, 0]) }, 'x,y,size are required');
 
 var qt = new QuadTree({x: x, y: y, size: size});
 
-assert.equal(qt.bounds[0], x);
-assert.equal(qt.bounds[1], y);
-assert.equal(qt.bounds[2], x + size);
-assert.equal(qt.bounds[3], y + size);
-assert.equal(qt.config.maxDepth, QuadTree.maxDepth);
-assert.equal(qt.config.maxFill, QuadTree.maxFill);
+assert.equal(x,        qt.bounds[0]);
+assert.equal(y,        qt.bounds[1]);
+assert.equal(x + size, qt.bounds[2]);
+assert.equal(y + size, qt.bounds[3]);
 
-var qt = new QuadTree({x: x, y: y, size: size, maxDepth: 1, maxFill: 1});
-assert.equal(qt.config.maxDepth, 1);
-assert.equal(qt.config.maxFill, 1);
+assert.equal(QuadTree.maxDepth, qt.config.maxDepth);
+assert.equal(QuadTree.maxFill,  qt.config.maxFill);
+
+var qt = new QuadTree({x: x, y: y, size: size, maxDepth: 2, maxFill: 1});
+assert.equal(2, qt.config.maxDepth);
+assert.equal(1, qt.config.maxFill);
+
+//because [] == [] returns false!?
+var res = qt.query([0, 0]);
+assert.equal('object', typeof res);
+assert.equal(0,        res.length);
+
+var res = qt.queryBounds([0, 0], [512, 512]);
+assert.equal('object', typeof res);
+assert.equal(0,        res.length);
+
+assert.equal(qt, qt.queryQuad([0, 0]));
+
+var obj = {foo: 'bar'};
+assert.equal(true, qt.insert([5, 5], {foo: 'bar'}));
+assert.equal(false, qt.insert([-5, -5], {foo: null}));
+
+var res = qt.query([0, 0]);
+assert.deepEqual([{key: [5, 5], value: obj}], res);
+assert.equal(1,     res.length);
+
+var res = qt.queryBounds([0, 0], [10, 10]);
+assert.deepEqual([{key: [5, 5], value: obj}], res);
+assert.equal(1,     res.length);
+
+
 
 console.log(qt);
